6a7728e5eb920089265e1bd55ab8a7ff217d83e6
GCB3220 <127167151+GCB3220@users.noreply.github.com>
2023-12-07 08:59:59 +0800

    Add files via upload

--- R1/week11_practice.R
+++ R1/week11_practice.R
@@ -3,14 +3,29 @@ field_A <- c(10.2, 10.7, 15.5, 10.4, 9.9, 10.0, 16.6,
              15.1, 15.2, 13.8, 14.1, 11.4, 11.5, 11.0)
 field_B <- c(8.1, 8.5, 8.4, 7.3, 8.0, 7.1, 13.9, 12.2, 
              13.4, 11.3, 12.6, 12.6, 12.7, 12.4, 11.3, 12.5)
-t.test(field_A, field_B, alternative = "greater")
-# p = 0.02 HA
+hist(field_A)
+hist(field_B)
+# 不能用t-test
+wilcox.test(field_A, field_B, alternative = "greater", paired = F)
+# H0
+
 
 # task 2: mouse
 mouse <- read.table("mice_weights.txt", head = T,  sep = ",")
-t.test(mouse$before, mouse$after, paired = TRUE)
-# p = 0.00000...1 HA
-
+summary(mouse)
+hist(mouse$before)
+hist(mouse$after)
+library(ggplot2)
+ggplot(data=mouse) +
+  geom_line(aes(x=before, col='before'), stat='density') +
+  geom_line(aes(x=after,col='after'), stat='density') + 
+  theme_classic()
+shapiro.test(mouse$before)
+var.test(mouse$before, mouse$after)
+# H0
+t.test(mouse$before, mouse$after, 
+       paired = TRUE, var.equal=TRUE)
+# HA
 
 
 
